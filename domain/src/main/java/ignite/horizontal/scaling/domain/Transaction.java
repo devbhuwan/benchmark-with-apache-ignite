095d56154fdea1c33567401c124e18bb9912b3f3
Bhuwan Prasad Upadhyay <comp.er.bhuwan@gmail.com>
2017-06-26 10:41:18 +0400

    project setup

---
+++ domain/src/main/java/ignite/horizontal/scaling/domain/Transaction.java
@@ -0,0 +1,18 @@
+package ignite.horizontal.scaling.domain;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+/**
+ * @author Bhuwan Upadhyay
+ * @date 2017/06/20
+ */
+public class Transaction implements Serializable {
+
+    private String reference;
+    private String type;
+    private BigDecimal amount;
+    private LocalDate date;
+
+}
